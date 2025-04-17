8df45468a72f888c086b8cb95a677e1cd32f64b4
sirisha <58425522+sirisha517@users.noreply.github.com>
2025-04-17 13:35:49 +0530

    complete

--- terraform-note/01-sample/test.tf
+++ terraform-note/01-sample/test.tf
@@ -4,6 +4,11 @@ provider "google" {
   zone    = "asia-south1-b"
 }
 
+# Reserve a static IP in the same region
+resource "google_compute_address" "static_ip" {
+  name   = "my-static-ip"
+  region = "asia-south1"
+}
 
 resource "google_compute_instance" "default" {
   name         = "my-instance"
@@ -13,24 +18,26 @@ resource "google_compute_instance" "default" {
   boot_disk {
     initialize_params {
       image = "centos-stream-9-v20250415"
+      size  = 30  # Set disk size to 30 GB
       labels = {
         my_label = "value"
       }
     }
   }
 
-  
-
   network_interface {
-  network    = "projects/co-vpc-host-prod-385510/global/networks/prod-base-vpc"
-  subnetwork = "projects/co-vpc-host-prod-385510/regions/asia-south1/subnetworks/sb-prod-shared-base-pub"
-  access_config {}  # Allows ephemeral public IP
-}
+    network    = "projects/co-vpc-host-prod-385510/global/networks/prod-base-vpc"
+    subnetwork = "projects/co-vpc-host-prod-385510/regions/asia-south1/subnetworks/sb-prod-shared-base-pub"
 
+    access_config {
+      nat_ip = google_compute_address.static_ip.address  # Use reserved static IP
+    }
+  }
 
   service_account {
     email  = "gcp-vm-reader@co-bharatgpt-prod.iam.gserviceaccount.com"
     scopes = ["cloud-platform"]
   }
 
+  depends_on = [google_compute_address.static_ip]
 }
