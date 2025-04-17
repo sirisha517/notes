391e382db655f1405c94d5df860e4a90ea993d46
sirisha <58425522+sirisha517@users.noreply.github.com>
2025-04-17 13:47:06 +0530

    v6

--- terraform-note/01-sample/test.tf
+++ terraform-note/01-sample/test.tf
@@ -4,25 +4,34 @@ provider "google" {
   zone    = "asia-south1-b"
 }
 
-# Reserve a static IP in the same region
+# Reserve a static external IP with PREMIUM network tier
 resource "google_compute_address" "static_ip" {
-  name   = "my-static-ip"
-  region = "asia-south1"
+  name         = "roboshop-demo-ip"
+  region       = "asia-south1"
+  network_tier = "PREMIUM"
 }
 
 resource "google_compute_instance" "default" {
-  name         = "my-instance"
+  name         = "roboshop-demo"
   machine_type = "e2-small"
   zone         = "asia-south1-b"
 
   boot_disk {
     initialize_params {
-      image = "centos-stream-9-v20250415"
-      size  = 30  # Set disk size to 30 GB
+      image = "ubuntu-minimal-2404-noble-amd64-v20240808"
+      size  = 30
+      type  = "pd-balanced" # Balanced Persistent Disk
       labels = {
         my_label = "value"
       }
     }
+
+    auto_delete = true
+
+    # Attach existing snapshot schedule
+    resource_policies = [
+      "projects/co-bharatgpt-prod/regions/asia-south1/resourcePolicies/default-schedule-1"
+    ]
   }
 
   network_interface {
@@ -30,9 +39,17 @@ resource "google_compute_instance" "default" {
     subnetwork = "projects/co-vpc-host-prod-385510/regions/asia-south1/subnetworks/sb-prod-shared-base-pub"
 
     access_config {
-      nat_ip = google_compute_address.static_ip.address  # Use reserved static IP
+      nat_ip       = google_compute_address.static_ip.address
+      network_tier = "PREMIUM"
     }
   }
+
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    # Install Google Cloud Ops Agent
+    curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+    sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+  EOF
 
   service_account {
     email  = "gcp-vm-reader@co-bharatgpt-prod.iam.gserviceaccount.com"
