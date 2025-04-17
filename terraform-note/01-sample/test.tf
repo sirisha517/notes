6a7fc04a9a0a9551f5341187fd58694f65adab99
sirisha <58425522+sirisha517@users.noreply.github.com>
2025-04-17 15:31:08 +0530

    zone

--- terraform-note/01-sample/test.tf
+++ terraform-note/01-sample/test.tf
@@ -13,7 +13,7 @@ resource "google_compute_address" "static_ip" {
 resource "google_compute_instance" "default" {
   name         = "my-instance"
   machine_type = "e2-small"
-  zone         = "asia-south1-b"
+  zone         = "asia-south1-a"
 
 # custom cpu and RAM
 # source "google_compute_instance" "default" {
