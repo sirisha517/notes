21f2ea0609debfa70e7051013a84b5a76666e24d
sirisha <58425522+sirisha517@users.noreply.github.com>
2025-04-17 13:54:32 +0530

    complete

--- terraform-note/01-sample/test.tf
+++ terraform-note/01-sample/test.tf
@@ -1,22 +1,38 @@
+provider "google" {
+  project = "co-bharatgpt-prod"
+  region  = "asia-south1"
+  zone    = "asia-south1-b"
+}
+
+# Static IP with PREMIUM network tier
+resource "google_compute_address" "static_ip" {
+  name         = "my-static-ip"
+  region       = "asia-south1"
+  address_type = "EXTERNAL"
+  network_tier = "PREMIUM"
+}
+
+# Boot Disk with Snapshot Schedule
+resource "google_compute_disk" "boot_disk" {
+  name  = "my-instance-boot-disk"
+  type  = "pd-balanced"
+  zone  = "asia-south1-b"
+  size  = 30
+  image = "centos-stream-9-v20250415"
+
+  resource_policies = [
+    "projects/co-bharatgpt-prod/regions/asia-south1/resourcePolicies/default-schedule-1"
+  ]
+}
+
+# VM Instance with custom disk and IP
 resource "google_compute_instance" "default" {
   name         = "my-instance"
   machine_type = "e2-small"
   zone         = "asia-south1-b"
 
   boot_disk {
-    initialize_params {
-      image = "centos-stream-9-v20250415"
-      size  = 30
-      type  = "pd-balanced"
-      labels = {
-        my_label = "value"
-      }
-    }
-
-    # ✅ Put this here (inside boot_disk, outside initialize_params)
-    resource_policies = [
-      "projects/co-bharatgpt-prod/regions/asia-south1/resourcePolicies/default-schedule-1"
-    ]
+    source = google_compute_disk.boot_disk.id
   }
 
   network_interface {
@@ -39,11 +55,4 @@ resource "google_compute_instance" "default" {
     curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
     bash add-google-cloud-ops-agent-repo.sh --also-install
   EOT
-}
-
-resource "google_compute_address" "static_ip" {
-  name         = "my-static-ip"
-  region       = "asia-south1"
-  address_type = "EXTERNAL"
-  network_tier = "PREMIUM"
 }
