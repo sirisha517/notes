3ef79baeaf3d6a98a1654640a07083b87242958a
sirisha <58425522+sirisha517@users.noreply.github.com>
2025-04-17 13:51:09 +0530

    last

--- terraform-note/01-sample/test.tf
+++ terraform-note/01-sample/test.tf
@@ -1,36 +1,23 @@
-provider "google" {
-  project = "co-bharatgpt-prod"
-  region  = "asia-south1"
-  zone    = "asia-south1-b"
-}
-
-# Reserve a static external IP with PREMIUM network tier
-resource "google_compute_address" "static_ip" {
-  name         = "roboshop-demo-ip"
-  region       = "asia-south1"
-  network_tier = "PREMIUM"
-}
-
 resource "google_compute_instance" "default" {
-  name         = "roboshop-demo"
+  name         = "my-instance"
   machine_type = "e2-small"
   zone         = "asia-south1-b"
 
   boot_disk {
-  initialize_params {
-    image = "centos-stream-9-v20250415"
-    size  = 30
-    type  = "pd-balanced"
-    labels = {
-      my_label = "value"
+    initialize_params {
+      image = "centos-stream-9-v20250415"
+      size  = 30
+      type  = "pd-balanced"
+      labels = {
+        my_label = "value"
+      }
     }
+
+    # ✅ Put this here (inside boot_disk, outside initialize_params)
+    resource_policies = [
+      "projects/co-bharatgpt-prod/regions/asia-south1/resourcePolicies/default-schedule-1"
+    ]
   }
-
-  resource_policies = [
-    "projects/co-bharatgpt-prod/regions/asia-south1/resourcePolicies/default-schedule-1"
-  ]
-}
-
 
   network_interface {
     network    = "projects/co-vpc-host-prod-385510/global/networks/prod-base-vpc"
@@ -42,17 +29,21 @@ resource "google_compute_instance" "default" {
     }
   }
 
-  metadata_startup_script = <<-EOF
-    #!/bin/bash
-    # Install Google Cloud Ops Agent
-    curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
-    sudo bash add-google-cloud-ops-agent-repo.sh --also-install
-  EOF
-
   service_account {
     email  = "gcp-vm-reader@co-bharatgpt-prod.iam.gserviceaccount.com"
     scopes = ["cloud-platform"]
   }
 
-  depends_on = [google_compute_address.static_ip]
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+    bash add-google-cloud-ops-agent-repo.sh --also-install
+  EOT
+}
+
+resource "google_compute_address" "static_ip" {
+  name         = "my-static-ip"
+  region       = "asia-south1"
+  address_type = "EXTERNAL"
+  network_tier = "PREMIUM"
 }
