c20fe5747787757464c698840301020c84bea93a
sirisha <58425522+sirisha517@users.noreply.github.com>
2025-04-17 13:58:28 +0530

    complete-v2

--- terraform-note/01-sample/test.tf
+++ terraform-note/01-sample/test.tf
@@ -19,17 +19,13 @@ resource "google_compute_disk" "boot_disk" {
   zone  = "asia-south1-b"
   size  = 30
   image = "centos-stream-9-v20250415"
+
+  resource_policies = [
+    "projects/co-bharatgpt-prod/regions/asia-south1/resourcePolicies/default-schedule-1"
+  ]
 }
 
-# Attach snapshot schedule using separate resource
-resource "google_compute_resource_policy_attachment" "snapshot_schedule" {
-  name           = "attach-snapshot-policy"
-  disk           = google_compute_disk.boot_disk.name
-  zone           = google_compute_disk.boot_disk.zone
-  resource_policy = "projects/co-bharatgpt-prod/regions/asia-south1/resourcePolicies/default-schedule-1"
-}
-
-# Create VM instance
+# Create the VM instance
 resource "google_compute_instance" "default" {
   name         = "my-instance"
   machine_type = "e2-small"
@@ -59,6 +55,4 @@ resource "google_compute_instance" "default" {
     curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
     bash add-google-cloud-ops-agent-repo.sh --also-install
   EOT
-
-  depends_on = [google_compute_resource_policy_attachment.snapshot_schedule]
 }
