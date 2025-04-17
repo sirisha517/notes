07e68f85575d14a55fcab5964b2dc6f662df1e2f
sirisha <58425522+sirisha517@users.noreply.github.com>
2025-04-17 15:04:14 +0530

    version

--- terraform-note/01-sample/test.tf
+++ terraform-note/01-sample/test.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.59.0"
+    }
+  }
+}
+
 provider "google" {
   project = "co-bharatgpt-prod"
   region  = "asia-south1"
